cf231264c883804b619c85da3f2d408db01a03d0
YourNorth <48354358+YourNorth@users.noreply.github.com>
2020-07-11 18:22:56 +0500

    creating simple hierarchy

---
+++ work_sercher/src/main/java/com/tenere_bufo/itis/WorkSercherApplication.java
@@ -0,0 +1,14 @@
+package com.tenere_bufo.itis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSearcherApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WorkSearcherApplication.class, args);
+    }
+
+}
---
+++ work_sercher/src/test/java/com/tenere_bufo/itis/WorkSercherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenere_bufo.itis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSercherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
