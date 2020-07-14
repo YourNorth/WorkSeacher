46858a3ae56cd90a50d7510a957afdcada2e67d5
Cupcake-master <bulat-cloud-1@yandex.ru>
2020-07-14 11:04:02 +0300

    Code optimization for job search

--- work_sercher/src/main/java/com/tenere_bufo/itis/controllers/FindJobForStudentsController.java
+++ work_sercher/src/main/java/com/tenere_bufo/itis/controllers/FindJobForStudentsController.java
@@ -39,48 +39,88 @@ public class FindJobForStudentsController {
 
     @PostMapping("/jobs")
     public String findJobsForStudents(Company company, Map<String, Object> model) {
-        model.put("companies", sortParameters(company));
+        model.put("companies", sortByParameters(company));
         System.out.println(company);
         return "jobs";
     }
 
-    private List<Company> sortParameters(Company company) {
+    private List<Company> sortByParameters(Company company) {
         List<Company> companies = companyRepository.findAll();
+        companies = sortByName(company, companies);
+        companies = sortByLocation(company, companies);
+        companies = sortByCategory(company, companies);
+        companies = sortByExperience(company, companies);
+        companies = sortByJobType(company, companies);
+        companies = sortByQualification(company, companies);
+        companies = sortByGender(company, companies);
+        companies = sortByAmount(company, companies);
+        return companies;
+    }
+
+    private List<Company> sortByName(Company company, List<Company> companies) {
         if (!company.getName().equals(DEFAULT_VALUE_FOR_NAME)) {
             companies = companies.stream().filter(s -> s.getName()
                     .equalsIgnoreCase(company.getName()))
                     .collect(Collectors.toList());
         }
+        return companies;
+    }
+
+    private List<Company> sortByLocation(Company company, List<Company> companies) {
         if (!company.getLocation().equals(DEFAULT_VALUE_FOR_LOCATION)) {
             companies = companies.stream().filter(s -> s.getLocation().equalsIgnoreCase(company.getLocation()))
                     .collect(Collectors.toList());
         }
-        if (!company.getCategory().equals(DEFAULT_VALUE_FOR_CATEGORY)){
+        return companies;
+    }
+
+    private List<Company> sortByCategory(Company company, List<Company> companies) {
+        if (!company.getCategory().equals(DEFAULT_VALUE_FOR_CATEGORY)) {
             companies = companies.stream().filter(s -> s.getCategory()
                     .equalsIgnoreCase(company.getCategory()))
                     .collect(Collectors.toList());
         }
-        if (!company.getExperience().equals(DEFAULT_VALUE_FOR_EXPERIENCE)){
+        return companies;
+    }
+
+    private List<Company> sortByExperience(Company company, List<Company> companies) {
+        if (!company.getExperience().equals(DEFAULT_VALUE_FOR_EXPERIENCE)) {
             companies = companies.stream().filter(s -> s.getExperience()
                     .equalsIgnoreCase(company.getExperience()))
                     .collect(Collectors.toList());
         }
-        if (!company.getJobType().equals(DEFAULT_VALUE_FOR_JOBTYPE)){
+        return companies;
+    }
+
+    private List<Company> sortByJobType(Company company, List<Company> companies) {
+        if (!company.getJobType().equals(DEFAULT_VALUE_FOR_JOBTYPE)) {
             companies = companies.stream().filter(s -> s.getJobType()
                     .equalsIgnoreCase(company.getJobType()))
                     .collect(Collectors.toList());
         }
-        if (!company.getQualification().equals(DEFAULT_VALUE_FOR_QUALIFICATION)){
+        return companies;
+    }
+
+    private List<Company> sortByQualification(Company company, List<Company> companies) {
+        if (!company.getQualification().equals(DEFAULT_VALUE_FOR_QUALIFICATION)) {
             companies = companies.stream().filter(s -> s.getQualification()
                     .equalsIgnoreCase(company.getQualification()))
                     .collect(Collectors.toList());
         }
-        if (!company.getGender().equals(DEFAULT_VALUE_FOR_GENDER)){
+        return companies;
+    }
+
+    private List<Company> sortByGender(Company company, List<Company> companies) {
+        if (!company.getGender().equals(DEFAULT_VALUE_FOR_GENDER)) {
             companies = companies.stream().filter(s -> s.getGender()
                     .equalsIgnoreCase(company.getGender()))
                     .collect(Collectors.toList());
         }
-        if (!company.getAmount().equals(DEFAULT_VALUE_FOR_AMOUNT)){
+        return companies;
+    }
+
+    private List<Company> sortByAmount(Company company, List<Company> companies) {
+        if (!company.getAmount().equals(DEFAULT_VALUE_FOR_AMOUNT)) {
             String input = company.getAmount();
             Pattern pattern = Pattern.compile("[ $-/Year]");
             String[] words = pattern.split(input);
