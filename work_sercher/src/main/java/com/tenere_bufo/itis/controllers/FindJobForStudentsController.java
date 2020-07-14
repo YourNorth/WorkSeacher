886a6ec6430cbdae700d475810bce4c29d467c9d
Cupcake-master <bulat-cloud-1@yandex.ru>
2020-07-14 10:50:22 +0300

    Added sorting by salary

--- work_sercher/src/main/java/com/tenere_bufo/itis/controllers/FindJobForStudentsController.java
+++ work_sercher/src/main/java/com/tenere_bufo/itis/controllers/FindJobForStudentsController.java
@@ -9,10 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Controller
 public class FindJobForStudentsController {
+    private final String DEFAULT_VALUE_FOR_NAME = "";
+    private final String DEFAULT_VALUE_FOR_LOCATION = "Location";
+    private final String DEFAULT_VALUE_FOR_CATEGORY = "Category";
+    private final String DEFAULT_VALUE_FOR_EXPERIENCE = "Experience";
+    private final String DEFAULT_VALUE_FOR_JOBTYPE = "Job type";
+    private final String DEFAULT_VALUE_FOR_QUALIFICATION = "Qualification";
+    private final String DEFAULT_VALUE_FOR_GENDER = "Gender";
+    private final String DEFAULT_VALUE_FOR_AMOUNT = "$750 - $24600/ Year";
 
     private final CompanyRepository companyRepository;
 
@@ -37,45 +46,53 @@ public class FindJobForStudentsController {
 
     private List<Company> sortParameters(Company company) {
         List<Company> companies = companyRepository.findAll();
-        if (!company.getName().equals("")) {
+        if (!company.getName().equals(DEFAULT_VALUE_FOR_NAME)) {
             companies = companies.stream().filter(s -> s.getName()
                     .equalsIgnoreCase(company.getName()))
                     .collect(Collectors.toList());
         }
-        if (!company.getLocation().equals("Location")) {
+        if (!company.getLocation().equals(DEFAULT_VALUE_FOR_LOCATION)) {
             companies = companies.stream().filter(s -> s.getLocation().equalsIgnoreCase(company.getLocation()))
                     .collect(Collectors.toList());
         }
-        if (!company.getCategory().equals("Category")){
+        if (!company.getCategory().equals(DEFAULT_VALUE_FOR_CATEGORY)){
             companies = companies.stream().filter(s -> s.getCategory()
                     .equalsIgnoreCase(company.getCategory()))
                     .collect(Collectors.toList());
         }
-        if (!company.getExperience().equals("Experience")){
+        if (!company.getExperience().equals(DEFAULT_VALUE_FOR_EXPERIENCE)){
             companies = companies.stream().filter(s -> s.getExperience()
                     .equalsIgnoreCase(company.getExperience()))
                     .collect(Collectors.toList());
         }
-        if (!company.getJobType().equals("Job type")){
+        if (!company.getJobType().equals(DEFAULT_VALUE_FOR_JOBTYPE)){
             companies = companies.stream().filter(s -> s.getJobType()
                     .equalsIgnoreCase(company.getJobType()))
                     .collect(Collectors.toList());
         }
-        if (!company.getQualification().equals("Qualification")){
+        if (!company.getQualification().equals(DEFAULT_VALUE_FOR_QUALIFICATION)){
             companies = companies.stream().filter(s -> s.getQualification()
                     .equalsIgnoreCase(company.getQualification()))
                     .collect(Collectors.toList());
         }
-        if (!company.getGender().equals("Gender")){
+        if (!company.getGender().equals(DEFAULT_VALUE_FOR_GENDER)){
             companies = companies.stream().filter(s -> s.getGender()
                     .equalsIgnoreCase(company.getGender()))
                     .collect(Collectors.toList());
         }
-//        if (!company.getAmount().equals("$750 - $24600/ Year")){
-//            String amount1 = company.getAmount().matches()
-//            String amoount2 = company.getAmount().substring(8, )
-//            companies = companies.stream().filter(s -> Integer.valueOf(s.getAmount().substring(1)) > )
-//        }
+        if (!company.getAmount().equals(DEFAULT_VALUE_FOR_AMOUNT)){
+            String input = company.getAmount();
+            Pattern pattern = Pattern.compile("[ $-/Year]");
+            String[] words = pattern.split(input);
+            int min = Integer.valueOf(words[1]);
+            int max = Integer.valueOf(words[5]);
+            companies = companies.stream().filter(s ->
+                    Integer.valueOf(s.getAmount().substring(1)) > min)
+                    .collect(Collectors.toList());
+            companies = companies.stream().filter(s ->
+                    Integer.valueOf(s.getAmount().substring(1)) < max)
+                    .collect(Collectors.toList());
+        }
         return companies;
     }
 }
