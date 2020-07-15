e5c9688150641039476ebb62d8b8721a78692435
Bilalov_Bulat <bulat-cloud-1@yandex.ru>
2020-07-15 12:26:26 +0300

    Changing index.ftl part 1

--- work_sercher/src/main/java/com/tenere_bufo/itis/controllers/IndexController.java
+++ work_sercher/src/main/java/com/tenere_bufo/itis/controllers/IndexController.java
@@ -1,15 +1,34 @@
 package com.tenere_bufo.itis.controllers;
 
 import com.tenere_bufo.itis.model.Company;
+import com.tenere_bufo.itis.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class IndexController {
 
+    private final CompanyService companyService;
+
+    @Autowired
+    public IndexController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
     @GetMapping("/index")
-    public String getIndex(){
+    public String getIndex(Map<String, Object> model){
+        List<Company> companiesAll = companyService.findAll();
+        List<Company> companies = IntStream.range(0, 5).mapToObj(companiesAll::get).collect(Collectors.toList());
+        model.put("companies", companies);
         return "index";
     }
 
