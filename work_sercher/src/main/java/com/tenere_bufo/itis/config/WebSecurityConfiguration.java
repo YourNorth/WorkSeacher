77633da02f1f271a883c9ec812b9803a252565d7
Bilalov_Bulat <bulat-cloud-1@yandex.ru>
2020-07-15 19:03:06 +0300

    Added filter instead of spring protection

--- work_sercher/src/main/java/com/tenere_bufo/itis/config/WebSecurityConfiguration.java
+++ work_sercher/src/main/java/com/tenere_bufo/itis/config/WebSecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.tenere_bufo.itis.config;
 
+import com.tenere_bufo.itis.filter.MainFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +16,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
                 .csrf().disable();
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+
+    @Bean
+    public FilterRegistrationBean<MainFilter> loggingFilter(){
+        FilterRegistrationBean<MainFilter> registrationBean
+                = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MainFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 }
