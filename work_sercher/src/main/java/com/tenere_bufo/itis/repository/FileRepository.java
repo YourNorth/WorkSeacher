19bfbc3ea2082adbb554c3d05eae35dcbd41f4a6
sirosh <sirosh@localhost.localdomain>
2020-07-22 09:03:31 +0300

    fixed another bug, that fails test

--- work_sercher/src/main/java/com/tenere_bufo/itis/repository/FileRepository.java
+++ work_sercher/src/main/java/com/tenere_bufo/itis/repository/FileRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<FileInfo, Long> {
-    Optional<FileInfo> findByUrl(String url);
+    //Optional<FileInfo> findByUrl(String url); 
 }
